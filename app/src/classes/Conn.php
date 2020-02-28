f5bc42d3156ebf69110a0ff31a5a4db855785dc5
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2020-02-28 15:16:54 -0300

    classe User completada

--- app/src/classes/Conn.php
+++ app/src/classes/Conn.php
@@ -1,23 +1,20 @@
 <?php
 
-# DB conf
-define('DB_SGDB', 'mysql');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'db_etec');
-define('DB_USER', 'tux');
-define('DB_PASS', '@MySQL2001');
+require '../config.php';
 
-class Conn {
+class DB
+{
 
-    public static function getConn() {
+    public static function getConn()
+    {
         try {
             $Conn = new PDO(DB_SGDB . ':host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
             $Conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $Conn->exec('SET CHARACTER SET utf8');
-            
+
             return $Conn;
-        } catch(PDO_Exception $e) {
+        } catch (PDOException $e) {
             echo $e->getMessage();
         }
-    } 
+    }
 }
--- app/src/classes/User.php
+++ app/src/classes/User.php
@@ -1,52 +1,70 @@
 <?php
 
-
-class User {
+class User
+{
     private $id;
     private $name;
     private $email;
     private $pass;
     private $access_level;
 
-    public function insert($User) { # C
+    public function insert($User)
+    { # C
         $query = "INSERT INTO tbusuario(nomeUsuario, emailUsuario, senhaUsuario, idNivelAcesso)
-                  VALUES ('". $User->name ."'
-                        , '". $User->email . "'
-                        , '". $User->senhaUsuario ."'
-                        , '". $User->access_level ."')
+                  VALUES ('$User->name'
+                        , '$User->email'
+                        , '$User->senhaUsuario'
+                        , '$User->access_level')
                  ";
-        
     }
 
-    public static function list() {        # R
+    public static function list()
+    { # R
         $query = "SELECT U.idUsuario, U.nomeUsuario, U.emailUsuario, N.nivel
                   FROM tbusuario AS U
                   INNER JOIN tbnivelacesso AS N
                   ON U.idNivelAcesso = N.idNivelAcesso";
-        
-        $DB = Conn::getConn();
 
-        $stmt = $DB->query($query);
+        $Conn = DB::getConn();
+
+        $stmt = $Conn->query($query);
         $result = $stmt->fetchAll();
-        
+
         return $result;
     }
 
-    public function update($User) { # U
+    public function update($User)
+    { # U
+        $query = "UPDATE tbusuario
+                  SET nomeUsuario = '$User->name',
+                      emailUsuario = '$User->email',
+                      senhaUsuario = '$User->pass',
+                      idNivelAcesso = '$User->access_level'
+                  WHERE idUsuario = $User->id
+                  ";
 
-    }
-
-    public function delete($id) { # D
-        $query = "DELETE FROM tbusuario
-                  WHERE idUsuario = " . $id . ";";
         try {
-            $DB = Conn::getConn();
-            $stmt = $DB->exec($query);
-            if($stmt->rowCount())
+            $Conn = DB::getConn();
+            $stmt = $Conn->exec($query);
+            if ($stmt->rowCount())
                 return 0;
-        } catch(PDOException $e) {
-            echo $e->getMessage();
+        } catch (PDOException $e) {
+            echo $e->getMessage;
         }
     }
 
+    public function delete($id)
+    { # D
+        $query = "DELETE FROM tbusuario
+                  WHERE idUsuario = $id;";
+
+        try {
+            $Conn = DB::getConn();
+            $stmt = $Conn->exec($query);
+            if ($stmt->rowCount())
+                return 0;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
 }
